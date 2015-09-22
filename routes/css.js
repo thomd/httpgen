5fe9a5e8009632b587cf3b90695b0e2ed130c6fa
Thomas Duerr <thomduerr@gmail.com>
2015-09-22 22:14:21 +0200

    set size of css file

--- routes/css.js
+++ routes/css.js
@@ -1,12 +1,27 @@
-var express = require('express');
-var router = express.Router();
-var sleep = require('sleep');
+'use strict';
 
-router.get('/:delay', function(req, res, next) {
-  sleep.sleep(parseInt(req.params.delay));
-  var css = '#foo .bar .baz { color: #F00; }';
+var express = require('express');
+var router  = express.Router();
+var sleep   = require('sleep');
+var css     = '#foo .bar .baz { color: #F00; }';
+
+
+router.get('/:delay/:size', function(req, res) {
+
+  // delay
+  var delay = parseInt(req.params.delay || 0);
+  res.set('X-Content-Delay', delay);
+  sleep.sleep(delay);
+
+  // size
+  var size = parseInt(req.params.size || css.length);
+  res.set('X-Content-Size', size);
+  var body = new Buffer(size);
+  body.fill(' ', body.write(css));
+
+  // response
   res.set('Content-Type', 'text/css');
-  res.send(css);
+  res.send(body.toString());
 });
 
 module.exports = router;
