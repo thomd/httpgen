38fc18dde5068cf1a9a59e13162b15f350cbdfd1
Thomas Duerr <thomduerr@gmail.com>
2015-09-30 21:38:05 +0200

    css test

--- test/test-css.js
+++ test/test-css.js
@@ -1,28 +1,33 @@
-var request = require('superagent');
+var request = require('request');
 var chai    = require('chai');
 var expect  = chai.expect;
 
-describe('css file', function() {
+describe('css response', function() {
 
-	var response;
+  var response, body;
 
-	before(function(done) {
-		request.get('localhost:5000/css/0/0/app.css').end(function(err, res) {
-			response = res;
-			done();
-		});
-	});
+  before(function(done) {
+    request.get('http://localhost:5000/css/0/1/app.css', function(error, _response, _body) {
+      response = _response;
+      body = _body;
+      done();
+    });
+  })
 
-	it('response is 200', function() {
-		expect(response.status).to.equal(200);
-	});
+  it('should have status 200', function() {
+      expect(response.statusCode).to.equal(200);
+  });
 
-	it('response type', function() {
-		expect(response.type).to.equal('text/css');
-	});
+  it('should be of content type css', function() {
+      expect(response.headers['content-type']).to.include('text/css');
+  });
 
-	it('response body', function() {
-		expect(response.text).include('background-color');
-	});
+  it('should have length of 1000 Bytes', function() {
+      expect(response.headers['content-length']).to.equal('1000');
+  });
+
+  it('should contain css code', function() {
+      expect(body).to.include('background-color');
+  });
 
 });
