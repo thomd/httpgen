c4924fe3dd6d9f006e0f2780b75d4c929b5474c2
Thomas Duerr <thomas.duerr@arvato.com>
2017-03-01 14:24:04 +0100

    milli-sec instead of sec

--- routes/javascript.js
+++ routes/javascript.js
@@ -4,7 +4,7 @@ var express = require('express');
 var router = express.Router();
 var util = require('util');
 //var javascript = 'function go(){if(go.count < %d){console.log(go.count++);window.setTimeout(go,1000);}}go.count=0;go();';
-var javascript = 'console.time("js execution");var result = 0;var now = new Date().getTime();while(new Date().getTime() < now + (%d * 1000)){result += Math.random() * Math.random()};console.timeEnd("js execution");';
+var javascript = 'console.time("js execution");var result = 0;var now = new Date().getTime();while(new Date().getTime() < now + (%d)){result += Math.random() * Math.random()};console.timeEnd("js execution");';
 
 router.get('/:delay?/:size?/:time?/:name.js', function(req, res) {
   var start = new Date();
@@ -22,7 +22,7 @@ router.get('/:delay?/:size?/:time?/:name.js', function(req, res) {
 
   // size: size of document in kilo-bytes
   var size = js.length;
-  size = Math.max(parseInt(req.params.size || 0) * 1000, size);
+  size = Math.max(parseInt(req.params.size || 0), size);
   res.set('X-Content-Size', size + ' kb');
   var body = new Buffer(size);
   body.fill(' ', body.write(js));
@@ -32,7 +32,7 @@ router.get('/:delay?/:size?/:time?/:name.js', function(req, res) {
     res.type('js');
     res.set('X-Response-Time', (new Date() - start) + ' ms');
     res.send(body.toString());
-  }, delay * 1000);
+  }, delay);
 });
 
 module.exports = router;
